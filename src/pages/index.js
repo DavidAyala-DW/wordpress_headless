7fa187a870b9a8f32dfceb98b17e5c5832107b48
DavidAyalaDW <luis.zanabria2@unmsm.edu.pe>
2023-04-10 16:07:37 -0500

    add custom styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,69 +4,21 @@ import Script from 'next/script'
 
 
 export default function Home({data, header}) {
-
-  const {content, title} = data;
   
   return (
 
-    <>
-
-      <Head>
-        <title>{title}</title>
-        <link
-          rel="preload"
-          href="https://i0.wp.com/kuruexpertscorner.com/wp-content/uploads/2023/02/understand-plantar-fascia.webp?fit=450%2C217&ssl=1"
-          as="image"
-        />
-      </Head>
-
-      <>
-        { parse(header) }
-      </>
-
-      <main>              
-
-        {
-          content && (
-            parse(content)
-          )
-        }
-
-        <Script src="/bundle.js"></Script>        
-
-      </main>
-    </>
+    <main className='p-10 bg-black text-white min-h-screen'>
+      <h1 className='text-xl text-center font-semibold'>Hello world</h1>
+    </main>
 
   )
 }
 
 export async function getStaticProps(context) {
 
-  const endpoint = process.env.WORDPRESS_SITE
-  const request = await fetch(`${endpoint}/graphql`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      query: `{
-        post(id: "2277", idType: DATABASE_ID) {
-          title,
-          content,
-        }
-      }`,
-    }),
-  });
-
-  const response = await request.json();
-  const data = response.data.post;
-
-  const header_request = await fetch(process.env.EDGE_FUNCTION);
-  const header_response = await header_request.json();
-  const {data:{ header }} = header_response;
 
   return {
-    props: {data, header}, // will be passed to the page component as props
+    props: {data: "hi"},
   }
 
 }

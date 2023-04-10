53bc1eb5933b08530cf77798fef972e9f51d7cfe
DavidAyalaDW <luis.zanabria2@unmsm.edu.pe>
2023-04-10 17:09:44 -0500

    add useState

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
 import parse from 'html-react-parser';
 import Head from 'next/head';
 import Script from 'next/script'
-
+import React, { useState } from 'react';
 
 export default function Home({data, header}) {
-  
+
+  const [count, setCount] = useState(0);
+
   return (
 
     <main className='p-10 bg-black text-white min-h-screen'>
       <h1 className='text-xl text-center font-semibold'>Hello world</h1>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </main>
 
   )

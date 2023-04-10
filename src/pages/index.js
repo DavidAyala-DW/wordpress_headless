259dc2c46ac4b6651e82e4006a5553375a07a93f
DavidAyalaDW <luis.zanabria2@unmsm.edu.pe>
2023-04-10 17:10:33 -0500

    Using heade

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,18 @@ export default function Home({data, header}) {
   const [count, setCount] = useState(0);
 
   return (
-
-    <main className='p-10 bg-black text-white min-h-screen'>
-      <h1 className='text-xl text-center font-semibold'>Hello world</h1>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </main>
-
+    <>
+      <Head>
+        <title>Test title</title>
+      </Head>
+      <main className='p-10 bg-black text-white min-h-screen'>
+        <h1 className='text-xl text-center font-semibold'>Hello world</h1>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+      </main>
+    </>
   )
 }
 

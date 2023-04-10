6dd0c2d4fe24c5ff1a373a7c557baab17aa2a1f2
DavidAyalaDW <luis.zanabria2@unmsm.edu.pe>
2023-04-10 17:06:46 -0500

    add next config

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.kurufootwear.com' : undefined,
+  assetPrefix: isProd ? 'https://wordpress-headless-david-ayala.vercel.app/' : undefined,
 }

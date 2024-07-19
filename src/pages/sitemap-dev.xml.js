f346a9b86388376d9c43ae510f2b7d5d4c70206b
DavidAyalaDW <luis.zanabria2@unmsm.edu.pe>
2024-07-19 18:02:18 -0500

    Sitemap test

---
+++ src/pages/sitemap-dev.xml.js
@@ -0,0 +1,41 @@
+import { XMLBuilder } from "fast-xml-parser";
+import { MongoClient } from "mongodb";
+const url = process.env.MONGO_URI;
+const dbName = "sitemap";
+
+export default function SitemapXml({ xml }) {
+  return xml;
+}
+
+export async function getServerSideProps({ res }) {
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection = db.collection("sitemaps");
+
+  const data = await collection.findOne({ _type: "index" });
+
+  const builder = new XMLBuilder({
+    ignoreAttributes: false,
+    attributeNamePrefix: "@@",
+    format: true,
+  });
+
+  const sitemap = JSON.parse(data?.xml);
+  const filteredSitemap = sitemap.sitemapindex.sitemap.filter(
+    (item) =>
+      !item.loc.includes("sitemap_pages_experts") &&
+      !item.loc.includes("sitemap_blog")
+  );
+  sitemap.sitemapindex.sitemap = filteredSitemap;
+
+  const xmlContent = builder.build(sitemap);
+  res.setHeader("Content-Type", "application/xml");
+  res.write(xmlContent);
+  res.end();
+
+  return {
+    props: {
+      xml: xmlContent,
+    },
+  };
+}

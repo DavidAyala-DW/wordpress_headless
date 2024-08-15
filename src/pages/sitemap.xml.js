658bd5de89abd1495c28fe1832607c549203e909
DavidAyalaDW <luis.zanabria2@unmsm.edu.pe>
2024-08-15 11:02:16 -0500

    New sitemap

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,41 +1,42 @@
-import { XMLBuilder} from "fast-xml-parser";
-import { MongoClient } from 'mongodb';
+import { XMLBuilder } from "fast-xml-parser";
+import { MongoClient } from "mongodb";
 const url = process.env.MONGO_URI;
-const dbName = 'sitemap';
+const dbName = "sitemap";
 
-export default function SitemapXml({xml}) {
+export default function SitemapXml({ xml }) {
   return xml;
 }
 
-export async function getServerSideProps({res}) {
-
+export async function getServerSideProps({ res }) {
   const client = await MongoClient.connect(url);
-  const db = client.db(dbName);    
+  const db = client.db(dbName);
   const collection = db.collection("sitemaps");
 
-  const data = await collection.findOne({_type : "index"});
+  const data = await collection.findOne({ _type: "index" });
 
   const builder = new XMLBuilder({
     ignoreAttributes: false,
     attributeNamePrefix: "@@",
-    format: true
+    format: true,
   });
 
   const sitemap = JSON.parse(data?.xml);
   const filteredSitemap = sitemap.sitemapindex.sitemap.filter(
     (item) =>
-      !item.loc.includes("sitemap_blogs")
+      !item.loc.includes("sitemap_pages_experts") &&
+      !item.loc.includes("/a/sitemap/sitemap_blog.xml") &&
+      !item.loc.includes("/a/sitemap/fr/sitemap_blog.xml")
   );
   sitemap.sitemapindex.sitemap = filteredSitemap;
 
   const xmlContent = builder.build(sitemap);
-  res.setHeader('Content-Type', 'application/xml');
-  res.write(xmlContent)
-  res.end()
-  
+  res.setHeader("Content-Type", "application/xml");
+  res.write(xmlContent);
+  res.end();
+
   return {
     props: {
-      xml: xmlContent
-    }
+      xml: xmlContent,
+    },
   };
 }
